9b867a06f380f5113d1de4b867efba95b82e6be0
Tae-hunKim <59422946+Tae-hunKim@users.noreply.github.com>
2021-03-31 17:49:00 +0900

    first commit

---
+++ src/main/java/com/atk/glasses/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.atk.glasses;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GlassesApplication.class);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>main</title>
+<script src="/js/jquery-3.6.0.js"></script>
+<script>
+    $(document).ready(function(){
+        alert('123');
+    });
+</script>
+</head>
+<body>
+Hello, SpringBoot 안녕하세요 api 123134444444
+</body>
+</html>
